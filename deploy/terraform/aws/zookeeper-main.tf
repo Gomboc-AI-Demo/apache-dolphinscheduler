34b214d6284d2eb2c2e78023cf7497623c3e67f0
pepegc <41898282+github-actions[bot]@users.noreply.github.com>
2026-06-17 19:28:12 +0000

    fix(gomboc): gomboc-ai/ensure-instance-metadata-service-version-1-is-not-enabled-for-aws-instance003 (dolphinscheduler-worker.tf, dolphinscheduler-standalone.tf, dolphinscheduler-master.tf, dolphinscheduler-api.tf, dolphinscheduler-alert.tf, zookeeper-main.tf)

--- deploy/terraform/aws/zookeeper-main.tf
+++ deploy/terraform/aws/zookeeper-main.tf
@@ -113,4 +113,7 @@ resource "aws_instance" "zookeeper" {
   })
   iam_instance_profile = "PLACEHOLDER_IAM_INSTANCE_PROFILE"
   disable_api_termination = true
+  metadata_options {
+    http_tokens = "required"
+  }
 }
