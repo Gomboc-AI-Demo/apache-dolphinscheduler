216402d73779da64c4af23ad9c293a18ab4d6bcb
pepegc <41898282+github-actions[bot]@users.noreply.github.com>
2026-06-17 20:08:07 +0000

    fix(gomboc): gomboc-ai/ensure-instance-api-termination-protection-is-enabled001 (dolphinscheduler-api.tf, dolphinscheduler-alert.tf, zookeeper-main.tf, dolphinscheduler-worker.tf, dolphinscheduler-standalone.tf, dolphinscheduler-master.tf)

--- deploy/terraform/aws/dolphinscheduler-standalone.tf
+++ deploy/terraform/aws/dolphinscheduler-standalone.tf
@@ -102,4 +102,5 @@ resource "aws_instance" "standalone_server" {
     "Name" = "${var.name_prefix}-standalone-${count.index}"
   })
   iam_instance_profile = "PLACEHOLDER_IAM_INSTANCE_PROFILE"
+  disable_api_termination = true
 }
