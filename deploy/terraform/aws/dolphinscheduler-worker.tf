84cbe1fb653e1ce502f50001da0892e0f49727a0
pepegc <41898282+github-actions[bot]@users.noreply.github.com>
2026-06-24 15:11:45 +0000

    fix(gomboc): gomboc-ai/finops/standard_storage_classes005 (dolphinscheduler-master.tf, dolphinscheduler-api.tf, dolphinscheduler-alert.tf, zookeeper-main.tf, dolphinscheduler-worker.tf, dolphinscheduler-standalone.tf)

--- deploy/terraform/aws/dolphinscheduler-worker.tf
+++ deploy/terraform/aws/dolphinscheduler-worker.tf
@@ -80,7 +80,7 @@ resource "aws_instance" "worker" {
 
   root_block_device {
     volume_size           = var.vm_root_volume_size.worker
-    volume_type           = var.vm_root_volume_type.worker
+    volume_type           = "gp3"
     delete_on_termination = true
     encrypted             = true
     tags = merge(var.tags, {
@@ -91,7 +91,7 @@ resource "aws_instance" "worker" {
   ebs_block_device {
     device_name           = "/dev/xvda"
     volume_size           = var.vm_data_volume_size.worker
-    volume_type           = var.vm_data_volume_type.worker
+    volume_type           = "gp3"
     encrypted             = true
     delete_on_termination = true
     tags = merge(var.tags, {
