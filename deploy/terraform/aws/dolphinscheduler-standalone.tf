d240374f35974bead45bab30e1a7546ac8a7ba4e
Pepe GC <jpgarcia90@gmail.com>
2026-07-02 10:35:49 -0400

    Update dolphinscheduler-standalone.tf

--- deploy/terraform/aws/dolphinscheduler-standalone.tf
+++ deploy/terraform/aws/dolphinscheduler-standalone.tf
@@ -15,6 +15,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
+
 resource "aws_security_group" "standalone" {
   name        = "standalone"
   description = "Allow incoming connections"
