51ad700b4450298eaf083f0eea35caea25612519
pepegc <41898282+github-actions[bot]@users.noreply.github.com>
2026-07-02 14:37:49 +0000

    fix(gomboc): gomboc-ai/terraform/aws/sentinel-ec2-instance-should-not-have-public-ip000 (zookeeper-main.tf, dolphinscheduler-worker.tf, dolphinscheduler-standalone.tf, dolphinscheduler-master.tf, dolphinscheduler-api.tf, dolphinscheduler-alert.tf)

--- deploy/terraform/aws/dolphinscheduler-standalone.tf
+++ deploy/terraform/aws/dolphinscheduler-standalone.tf
@@ -74,7 +74,7 @@ resource "aws_instance" "standalone_server" {
   subnet_id                   = aws_subnet.public[0].id
   vpc_security_group_ids      = [aws_security_group.standalone.id]
   source_dest_check           = false
-  associate_public_ip_address = var.vm_associate_public_ip_address.standalone_server
+  associate_public_ip_address = false
 
   user_data = data.template_file.standalone_user_data.rendered
 
