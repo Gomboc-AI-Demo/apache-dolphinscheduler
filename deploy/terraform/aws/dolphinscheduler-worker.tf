b84c6db00634f25f83ba18f76f579fa3e3ac22fa
pepegc <41898282+github-actions[bot]@users.noreply.github.com>
2026-07-10 21:03:54 +0000

    fix(gomboc): gomboc-ai/finops/standard_storage_classes006 (dolphinscheduler-alert.tf, zookeeper-main.tf, dolphinscheduler-worker.tf, dolphinscheduler-standalone.tf, dolphinscheduler-master.tf, dolphinscheduler-api.tf)

--- deploy/terraform/aws/dolphinscheduler-worker.tf
+++ deploy/terraform/aws/dolphinscheduler-worker.tf
@@ -79,7 +79,7 @@ resource "aws_instance" "worker" {
   user_data = data.template_file.worker_user_data.rendered
 
   root_block_device {
-    volume_size           = var.vm_root_volume_size.worker
+    volume_size           = 50
     volume_type           = "gp3"
     delete_on_termination = true
     encrypted             = true
@@ -90,7 +90,7 @@ resource "aws_instance" "worker" {
 
   ebs_block_device {
     device_name           = "/dev/xvda"
-    volume_size           = var.vm_data_volume_size.worker
+    volume_size           = 50
     volume_type           = "gp3"
     encrypted             = true
     delete_on_termination = true
--- deploy/terraform/aws/zookeeper-main.tf
+++ deploy/terraform/aws/zookeeper-main.tf
@@ -73,7 +73,7 @@ resource "aws_instance" "zookeeper" {
   user_data = data.template_file.zookeeper_user_data.rendered
 
   root_block_device {
-    volume_size           = var.vm_root_volume_size.standalone_server
+    volume_size           = 50
     volume_type           = "gp3"
     delete_on_termination = true
     encrypted             = true
@@ -84,7 +84,7 @@ resource "aws_instance" "zookeeper" {
 
   ebs_block_device {
     device_name           = "/dev/xvda"
-    volume_size           = var.vm_data_volume_size.standalone_server
+    volume_size           = 50
     volume_type           = "gp3"
     encrypted             = true
     delete_on_termination = true
