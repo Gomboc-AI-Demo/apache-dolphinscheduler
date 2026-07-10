a8d9e955654aa4c338aa7580b0b9b6503fdca44c
pepegc <41898282+github-actions[bot]@users.noreply.github.com>
2026-07-10 21:03:54 +0000

    fix(gomboc): gomboc-ai/finops/standard_storage_classes005 (dolphinscheduler-worker.tf, dolphinscheduler-standalone.tf, dolphinscheduler-master.tf, dolphinscheduler-api.tf, dolphinscheduler-alert.tf, zookeeper-main.tf)

--- deploy/terraform/aws/dolphinscheduler-alert.tf
+++ deploy/terraform/aws/dolphinscheduler-alert.tf
@@ -79,7 +79,7 @@ resource "aws_instance" "alert" {
 
   root_block_device {
     volume_size           = var.vm_root_volume_size.alert
-    volume_type           = var.vm_root_volume_type.alert
+    volume_type           = "gp3"
     delete_on_termination = true
     encrypted             = true
     tags = merge(var.tags, {
@@ -90,7 +90,7 @@ resource "aws_instance" "alert" {
   ebs_block_device {
     device_name           = "/dev/xvda"
     volume_size           = var.vm_data_volume_size.alert
-    volume_type           = var.vm_data_volume_type.alert
+    volume_type           = "gp3"
     encrypted             = true
     delete_on_termination = true
     tags = merge(var.tags, {
--- deploy/terraform/aws/dolphinscheduler-api.tf
+++ deploy/terraform/aws/dolphinscheduler-api.tf
@@ -79,7 +79,7 @@ resource "aws_instance" "api" {
 
   root_block_device {
     volume_size           = var.vm_root_volume_size.api
-    volume_type           = var.vm_root_volume_type.api
+    volume_type           = "gp3"
     delete_on_termination = true
     encrypted             = true
     tags = merge(var.tags, {
@@ -90,7 +90,7 @@ resource "aws_instance" "api" {
   ebs_block_device {
     device_name           = "/dev/xvda"
     volume_size           = var.vm_data_volume_size.api
-    volume_type           = var.vm_data_volume_type.api
+    volume_type           = "gp3"
     encrypted             = true
     delete_on_termination = true
     tags = merge(var.tags, {
