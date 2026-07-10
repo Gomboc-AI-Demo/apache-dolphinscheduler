5d60c4ec087c072d351426a08e2ee75048d93d9d
pepegc <41898282+github-actions[bot]@users.noreply.github.com>
2026-07-10 21:03:53 +0000

    fix(gomboc): gomboc-ai/terraform/aws/sentinel-deny-public-rdp-acl-rules000 (dolphinscheduler-worker.tf, dolphinscheduler-standalone.tf, dolphinscheduler-master.tf, dolphinscheduler-api.tf, dolphinscheduler-alert.tf, zookeeper-main.tf)

--- deploy/terraform/aws/zookeeper-main.tf
+++ deploy/terraform/aws/zookeeper-main.tf
@@ -38,7 +38,7 @@ resource "aws_security_group" "zookeeper_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["USER_INPUT_1"]
     description = "Allow incoming SSH connections (Linux)"
   }
   egress {
