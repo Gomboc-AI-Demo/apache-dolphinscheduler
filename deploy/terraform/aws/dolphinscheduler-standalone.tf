bbf84a7a54f291f635873f4734d20a823293f24f
ztalarick <41898282+github-actions[bot]@users.noreply.github.com>
2026-07-22 18:24:24 +0000

    fix(gomboc): gomboc-ai/terraform/aws/sentinel-ec2-instance-profile-required000 (dolphinscheduler-worker.tf, dolphinscheduler-standalone.tf, dolphinscheduler-master.tf, dolphinscheduler-api.tf, dolphinscheduler-alert.tf, zookeeper-main.tf)

--- deploy/terraform/aws/dolphinscheduler-standalone.tf
+++ deploy/terraform/aws/dolphinscheduler-standalone.tf
@@ -101,4 +101,5 @@ resource "aws_instance" "standalone_server" {
   tags = merge(var.tags, {
     "Name" = "${var.name_prefix}-standalone-${count.index}"
   })
+  iam_instance_profile = "PLACEHOLDER_IAM_INSTANCE_PROFILE"
 }
